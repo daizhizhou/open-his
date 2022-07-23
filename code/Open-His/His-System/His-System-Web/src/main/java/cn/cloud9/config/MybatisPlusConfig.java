8588683fe1791297af91856209f7823559828e52
cloud9 <1791255334@qq.com>
2022-07-23 16:40:58 +0800

    提交MybatisPlus分页支持

---
+++ code/Open-His/His-System/His-System-Web/src/main/java/cn/cloud9/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package cn.cloud9.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+/**
+ * @author OnCloud9
+ * @description
+ * @project Open-His
+ * @date 2022年07月23日 下午 02:47
+ */
+@EnableTransactionManagement
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+}
