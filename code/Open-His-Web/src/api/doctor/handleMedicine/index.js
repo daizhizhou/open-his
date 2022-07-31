a552d6e920de5b007fca9645f616c985de8d0b20
cloud9 <1791255334@qq.com>
2022-07-31 14:27:58 +0800

    修复发药更新BUG

--- code/Open-His-Web/src/api/doctor/handleMedicine/index.js
+++ code/Open-His-Web/src/api/doctor/handleMedicine/index.js
@@ -6,6 +6,13 @@ export function getChargedCareHistoryOnlyMedicinesByRegId(regId) {
     method: 'get'
   })
 }
+// 根据挂号ID查询已支付的药品处方信息及详情
+export function getNoChargeCareHistoryByRegId(regId) {
+  return request({
+    url: '/doctor/handleMedicine/getChargedCareHistoryOnlyMedicinesByRegId/' + regId,
+    method: 'get'
+  })
+}
 // 处理发药
 export function doMedicine(data) {
   return request({
